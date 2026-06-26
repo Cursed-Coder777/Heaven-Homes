5eb1f4235658607f1d916b7224a7f4e7aa3e35ca
coder <cursedcoder812@gmail.com>
2026-06-26 23:21:15 +0500

    chore: whitelist assets.codepen.io for next/image remote patterns

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.codepen.io",
+      },
+    ],
+  },
+};
 
 export default config;
